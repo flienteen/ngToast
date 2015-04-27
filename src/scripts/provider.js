7d69636d194505c941628f431e410cb739fd9598
flienteen <flienteen@gmail.com>
2015-04-27 17:25:45 +0300

    refactoring

--- src/scripts/provider.js
+++ src/scripts/provider.js
@@ -1,92 +1,118 @@
-(function(window, angular, undefined) {
-  'use strict';
+(function(window, angular, undefined)
+{
+	'use strict';
 
-  angular.module('ngToast.provider', [])
-    .provider('ngToast', [
-      function() {
-        var messages = [],
-            messageStack = [];
+	angular
+		.module('ngToast.provider', [])
+		.provider('ngToast', [ngToastProvider])
+	;
 
-        var defaults = {
-          animation: false,
-          className: 'success',
-          additionalClasses: null,
-          dismissOnTimeout: true,
-          timeout: 4000,
-          dismissButton: false,
-          dismissButtonHtml: '&times;',
-          dismissOnClick: true,
-          compileContent: false,
-          horizontalPosition: 'right', // right, center, left
-          verticalPosition: 'top', // top, bottom,
-          maxNumber: 0
-        };
 
-        function Message(msg) {
-          var id = Math.floor(Math.random()*1000);
-          while (messages.indexOf(id) > -1) {
-            id = Math.floor(Math.random()*1000);
-          }
+	function ngToastProvider()
+	{
+		var
+			messages = [],
+			messageStack = [],
 
-          this.id = id;
-          this.animation = defaults.animation;
-          this.className = defaults.className;
-          this.additionalClasses = defaults.additionalClasses;
-          this.dismissOnTimeout = defaults.dismissOnTimeout;
-          this.timeout = defaults.timeout;
-          this.dismissButton = defaults.dismissButton;
-          this.dismissButtonHtml = defaults.dismissButtonHtml;
-          this.dismissOnClick = defaults.dismissOnClick;
-          this.compileContent = defaults.compileContent;
+			defaults = {
+				animation: false,
+				className: 'success',
+				additionalClasses: null,
+				dismissOnTimeout: true,
+				timeout: 4000,
+				dismissButton: false,
+				dismissButtonHtml: '&times;',
+				dismissOnClick: true,
+				compileContent: false,
+				horizontalPosition: 'right', // right, center, left
+				verticalPosition: 'top', // top, bottom,
+				maxNumber: 0
+			}
+		;
 
-          angular.extend(this, msg);
-        }
+		this.configure = configure;
+		this.$get = [$get];
 
-        this.configure = function(config) {
-          angular.extend(defaults, config);
-        };
 
-        this.$get = [function() {
-          return {
-            settings: defaults,
-            messages: messages,
-            dismiss: function(id) {
-              if (id) {
-                for (var i = messages.length - 1; i >= 0; i--) {
-                  if (messages[i].id === id) {
-                    messages.splice(i, 1);
-                    messageStack.splice(messageStack.indexOf(id), 1);
-                    return;
-                  }
-                }
+		////
+		function configure(config)
+		{
+			angular.extend(defaults, config);
+		}
 
-              } else {
-                while(messages.length > 0) {
-                  messages.pop();
-                }
-                messageStack = [];
-              }
-            },
-            create: function(msg) {
-              if (defaults.maxNumber > 0 &&
-                  messageStack.length >= defaults.maxNumber) {
-                this.dismiss(messageStack[0]);
-              }
+		function $get()
+		{
+			return {
+				settings: defaults,
+				messages: messages,
+				dismiss: dismiss,
+				create: create
+			};
+		}
 
-              msg = (typeof msg === 'string') ? {content: msg} : msg;
+		function dismiss(id)
+		{
+			if (id)
+			{
+				for (var i = messages.length - 1; i>=0; i--)
+				{
+					if (messages[i].id === id)
+					{
+						messages.splice(i, 1);
+						messageStack.splice(messageStack.indexOf(id), 1);
+						return;
+					}
+				}
 
-              var newMsg = new Message(msg);
-              if (defaults.verticalPosition === 'bottom') {
-                messages.unshift(newMsg);
-              } else {
-                messages.push(newMsg);
-              }
-              messageStack.push(newMsg.id);
-              return newMsg.id;
-            }
-          };
-        }];
-      }
-    ]);
+			} else {
+				while (messages.length>0) {
+					messages.pop();
+				}
+				messageStack = [];
+			}
+		}
+
+		function create(msg)
+		{
+			if (defaults.maxNumber>0 && messageStack.length>=defaults.maxNumber) {
+				this.dismiss(messageStack[0]);
+			}
+
+			msg = (typeof msg === 'string') ? {content:msg} : msg;
+
+			var newMsg = new Message(msg);
+			if (defaults.verticalPosition === 'bottom') {
+				messages.unshift(newMsg);
+			} else {
+				messages.push(newMsg);
+			}
+			messageStack.push(newMsg.id);
+
+			return newMsg.id;
+		}
+
+		function Message(msg)
+		{
+			var id = Math.floor(Math.random() * 1000);
+
+			while (messages.indexOf(id)> -1) {
+				id = Math.floor(Math.random() * 1000);
+			}
+
+			this.id = id;
+			this.animation = defaults.animation;
+			this.className = defaults.className;
+			this.additionalClasses = defaults.additionalClasses;
+			this.dismissOnTimeout = defaults.dismissOnTimeout;
+			this.timeout = defaults.timeout;
+			this.dismissButton = defaults.dismissButton;
+			this.dismissButtonHtml = defaults.dismissButtonHtml;
+			this.dismissOnClick = defaults.dismissOnClick;
+			this.compileContent = defaults.compileContent;
+
+			angular.extend(this, msg);
+		}
+
+	}
 
 })(window, window.angular);
